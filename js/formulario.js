6d8d2b03610028b4e183947e8f4c2345c1aafe2f
destellito <jomunozmol@gmail.com>
2023-05-16 20:21:47 -0400

    FormularioDeRegistro-v1
    Formulario de registro añadido, usa javascript, 2 funciones, ocultar y mostrar, valida cuando faltan datos o se escribe mal un email

---
+++ js/formulario.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $("#mostrarBtn").click(function() {
+        $("#formulario").show(500);
+    });
+});
+
+$(document).ready(function() {
+    $("#ocultarBtn").click(function() {
+        $("#formulario").hide(500);
+    });
+});  
+
+document.getElementById("formulario").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    var nombre = document.getElementById("nombre").value;
+    var apellidoPaterno = document.getElementById("apellidoPaterno").value;
+    var apellidoMaterno = document.getElementById("apellidoMaterno").value;
+    var rut = document.getElementById("rut").value;
+    var direccion = document.getElementById("direccion").value;
+    var telefono = document.getElementById("telefono").value;
+    var email = document.getElementById("email").value;
+    var genero = document.getElementById("genero").value;
+
+    if (nombre === "" || apellidoPaterno === "" || apellidoMaterno === "" || rut === "" || direccion === "" || telefono === "" || email === "" || genero === "") {
+    alert("Por favor, completa todos los campos");
+    return;
+    }
+
+    document.getElementById("miFormulario").submit();
+
+});
