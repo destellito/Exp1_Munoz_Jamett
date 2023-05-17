78341545748fcbac2f6add86c2d3dc347aa3f00d
destellito <jomunozmol@gmail.com>
2023-05-16 21:25:59 -0400

    orden-de-paginas-y-validaciones
    Orden de páginas al cambiar entre ellas ahora se pone en primer lugar la página en la que se está, también se añadieron validaciones al rut y el número telefónico

--- js/formulario.js
+++ js/formulario.js
@@ -21,8 +21,27 @@ document.getElementById("formulario").addEventListener("submit", function(event)
     var genero = document.getElementById("genero").value;
 
     if (nombre === "" || apellidoPaterno === "" || apellidoMaterno === "" || rut === "" || direccion === "" || telefono === "" || email === "" || genero === "") {
-    alert("Por favor, completa todos los campos");
-    return;
+        alert("Por favor, completa todos los campos");
+        event.preventDefault();
+        return;
+    }
+    //validar que el rut contenga guión antes del último caracter
+    if (rut.charAt(rut.length - 2) !== "-") {
+        alert("El rut debe contener un guión");
+        event.preventDefault();
+        return;
+    }
+    //validar que el rut no se escriba con puntos
+    if (rut.indexOf(".") !== -1) {
+        alert("El rut no debe contener puntos");
+        event.preventDefault();
+        return;
+    }
+    //validar que el numero de telefono no supere los 9 dígitos
+    if (telefono.length > 9) {
+        alert("El número de telefono ingresado no sigue un formato válido")
+        event.preventDefault();
+        return;
     }
 
     alert("¡Registrado Correctamente!")    
