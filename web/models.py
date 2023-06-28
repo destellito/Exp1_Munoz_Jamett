bfc16e9d81b97aa047dd69c6fb71f2e5769cf44e
valjamettduoc <108497801+valjamettduoc@users.noreply.github.com>
2023-06-27 21:09:52 -0400

    creación clase producto en models.py
    class Producto creado

--- web/models.py
+++ web/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 
 # CLASS PRODUCTO!!
+
+
+
+class Producto(models.Model):
+	idproducto=models.AutoField(db_column='idProducto', primary_key=True) 
+    nombre=models.CharField(max_length=50)
+	precio=models.IntergerField()
+	imagen=models.ImageField(upload_to='productos/')    
